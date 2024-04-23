3a2b53e07d8ec269df53c6268542cafadc69b4ad
kaushik-abhishek <vyasdusani@gmail.com>
2024-04-23 21:39:42 +0530

    Timer ADD

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useReducer } from "react";
 import { useEffect } from "react";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
+import Timer from "./components/Timer";
 
 const initialState = {
   questions: [],
@@ -13,13 +14,16 @@ const initialState = {
   secondsRemaining: null,
 };
 
+const SECS_PER_QUESTION = 30;
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case "dataReceived":
+    case 'dataReceived':
       return {
         ...state,
         questions: action.payload,
-        status: "ready",
+        status: 'ready',
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION
       };
     case "newAnswer":
       return {
@@ -37,6 +41,12 @@ const reducer = (state, action) => {
         ...state,
         status: "finish",
       };
+      case 'tik':
+        return{
+        ...state,
+        secondsRemaining:state.secondsRemaining-1,
+        status:state.secondsRemaining===0?'finished':state.status,
+        }
   }
 };
 
@@ -62,6 +72,7 @@ function App() {
         />
       )}
       <footer className="footer">
+        <Timer secondsRemaining={secondsRemaining} dispatch={dispatch} />
         <NextButton
           numQuestions={numQuestions}
           index={index}
@@ -69,6 +80,7 @@ function App() {
           dispatch={dispatch}
         />
       </footer>
+      
     </div>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const secs = secondsRemaining % 60;
+
+  useEffect(() => {
+     const id = setInterval(() => {
+          dispatch({type: 'tik'})
+     }, 1000)
+
+     return () => clearInterval(id);
+  },[dispatch])
+
+  return (
+  <div>
+     {mins < 10 && "0"}{mins}:{secs < 10 && "0"}{secs}
+  </div>);
+}
